2d5b7c66a40c40ff9023cf9fbaa04138fb978c39
Kristoffer Sahlin <kxs624@psu.edu>
2017-05-17 16:46:29 -0400

    some updates. Still need to figure out why all hist are so bad to database

--- analysis/isoform_level/get_FN_with_respect_to_original_flnc.py
+++ analysis/isoform_level/get_FN_with_respect_to_original_flnc.py
@@ -1,9 +1,12 @@
-
+import argparse
+import os, sys
 
 def get_best_hits(file_name):
     best_hits = {}
     for line in open(file_name):
-        query, target, ed = line.strip().split()
+        # print(line)
+        # print(line.strip().split("\t"))
+        query, target, len_query, len_target, ed = line.strip().split("\t")
         ed = int(ed)
         if target in best_hits:
             if ed < best_hits[target]:
@@ -17,19 +20,37 @@ def main(args):
     
     read_hits = get_best_hits(args.best_hits_reads)
     predicted_hits = get_best_hits(args.best_hits_pred)
+    print(len(read_hits))
+    print(len(predicted_hits))
+
     FN = {}
     for db_hit in read_hits:
         if db_hit not in predicted_hits:
             FN[db_hit] = read_hits[db_hit]
+            print("not found:", db_hit, read_hits[db_hit])
         elif predicted_hits[db_hit] > read_hits[db_hit]:
-            print("read had better hit")
+            print("read had better hit", predicted_hits[db_hit], read_hits[db_hit], db_hit)
             FN[db_hit] = read_hits[db_hit]
+        elif predicted_hits[db_hit] == read_hits[db_hit]:
+            print("Same quality hit", predicted_hits[db_hit], read_hits[db_hit], db_hit)
         else:
+            # print("predicted had better hit", predicted_hits[db_hit], read_hits[db_hit])
             continue
 
     print("False negatives")
-    for fn in FN:
-        print(FN[fn], fn)
+    # for fn in FN:
+    #     print(FN[fn], fn)
+    better_hit_to_pred = {}
+    for db_hit in predicted_hits:
+        if db_hit not in read_hits:
+            better_hit_to_pred[db_hit] = predicted_hits[db_hit]
+            print("not found in reads:", db_hit, better_hit_to_pred[db_hit])
+        elif predicted_hits[db_hit] < read_hits[db_hit]:
+            print("Predicted had better hit", predicted_hits[db_hit], read_hits[db_hit], db_hit)
+            better_hit_to_pred[db_hit] = predicted_hits[db_hit]
+        else:
+            # print("read had better hit", predicted_hits[db_hit], read_hits[db_hit])
+            continue
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Align predicted transcripts to transcripts in ensembl reference data base.")
@@ -42,6 +63,5 @@ if __name__ == '__main__':
     if not os.path.exists(args.outfolder):
         os.makedirs(args.outfolder)
     
-    if args.database:
-    else:
-        main(args)
+
+    main(args)
