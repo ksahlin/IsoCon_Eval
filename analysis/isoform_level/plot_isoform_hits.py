056139b343374bb5b81bba78a280548262739eb4
Kristoffer Sahlin <kxs624@psu.edu>
2017-08-28 14:22:51 -0400

    fix

--- analysis/isoform_level/plot_isoform_hits.py
+++ analysis/isoform_level/plot_isoform_hits.py
@@ -60,8 +60,17 @@ def get_best_hits_over_identity_threshold(file_names, targeted, args):
                 smallest_string_acc_target = target.split(",")[0]
 
                 if smallest_string_acc_target in best_hits:
-                    if ed < best_hits[smallest_string_acc_target]:
+                    if ed < best_hits[smallest_string_acc_target][0]:
                         best_hits[smallest_string_acc_target] = (ed, clipped)
+                        
+                    elif ed == best_hits[smallest_string_acc_target][0]:
+
+                        if clipped < best_hits[smallest_string_acc_target][1]:
+                            best_hits[smallest_string_acc_target] = (ed, clipped)
+                        else:
+                            print("same hit here")
+                    else:
+                        print("Hit with larger ed:", ed,  best_hits[smallest_string_acc_target][0])
 
                 else:
                     best_hits[smallest_string_acc_target] = (ed, clipped)
